396e600cd60d2e4eb7b447c328dfb0a9c26e9fcd
luana-martins <lmartins.comp@gmail.com>
2022-11-28 11:27:10 -0300

    split conditional

--- src/test/java/StringUtilsTestUnit5.java
+++ src/test/java/StringUtilsTestUnit5.java
@@ -112,6 +112,6 @@ public class StringUtilsTestUnit5 {
   @Test
   public void testConcatWithAllNullInput() {
     String actual = MinhaStringUtils.concat();
-    assertFalse(actual != null);
+    assertEquals(actual, null);
   }
 }
